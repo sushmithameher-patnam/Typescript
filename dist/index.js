651289ebc7b01f409281aa751efd926c17412cd0
mehersushu <patnamsushmithameher@gmail.com>
2024-07-30 17:26:28 -0500

    Typescript basic configurations and debugging

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello");
+let age = 27;
+if (age > 20)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello");
+let age: number=27;
+//age='hello'
+if(age>20)
+    age+=10
+console.log(age)
